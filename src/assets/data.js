709001d95104e0ab2c37343c5eed49cf9376a259
Lourdes <lourdes.jupor>
2024-11-04 12:43:12 +0100

    Estructura de la card y datos

---
+++ src/assets/data.js
@@ -0,0 +1,27 @@
+export const cardData = [
+  {
+    title: "LANDING PAGE",
+    description: "Presenta tu negocio con una Landing Page profesional. Comunica tus servicios y valores con un diseño moderno que refleja tu marca.",
+    large_description_title: "¿Qué incluye?",
+    large_description_items: [
+    "Diseño de una sola página.",
+    "Información sobre el producto o promoción.",
+    "Formularios de contacto.",
+    "Sobre nosotros.",
+    "Enlaces a tus redes sociales."
+    ]
+  },
+  {
+    title: "PORTFOLIO WEB",
+    description: "Muestra tu talento de forma profesional con un Portfolio Web personalizado. Ideal para destacar tus proyectos y atraer más clientes.",
+    large_description_title: "¿Qué incluye?",
+    large_description_items: [
+    "Sobre mi/Sobre nosotros",
+    "Trabajos/ productos",
+    "Galería de imágenes " ,
+    "Información sobre los proyectos",
+    "Contacto",
+
+    ]
+  },
+];
