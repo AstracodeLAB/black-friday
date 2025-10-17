a96aaa4fa741eccad392d042d2915f11cd2581a3
MariaRipley <roblesb.maria@gmail.com>
2025-10-17 16:40:24 +0200

    Finish new design

--- src/assets/data.js
+++ src/assets/data.js
@@ -3,26 +3,5 @@ export const cardData = [
     title: '',
     description:
       'En AstracodeLAB celebramos el Black Friday con un 20% de descuento en nuestros servicios de landing page y portfolios web. Obtén tu código de descuento haciendo click en el botón y canjéalo antes de que el contador se ponga a 0.',
-    // large_description_title: '¿Qué incluye la Landing Page?',
-    // large_description_items: [
-    //   '• Diseño de una sola página.',
-    //   '• Información sobre el producto o promoción.',
-    //   '• Formularios de contacto.',
-    //   '• Sobre nosotros.',
-    //   '• Enlaces a tus redes sociales.',
-    // ],
   },
-  // {
-  //   title:"PORTFOLIO WEB",
-  //   description: "Muestra tu talento de forma profesional con un Portfolio Web personalizado. Ideal para destacar tus proyectos y atraer más clientes.",
-  //   large_description_title: "¿Qué incluye el Portfolio Web?",
-  //   large_description_items: [
-  //   "• Sobre mi/Sobre nosotros",
-  //   "• Trabajos/ productos",
-  //   "• Galería de imágenes " ,
-  //   "• Información sobre los proyectos",
-  //   "• Contacto",
-
-  //   ]
-  // },
 ];
